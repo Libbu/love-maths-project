f16d1bb712a099909477d9e32c8b9fa1da1a8a78
Libbu Libbu <elizabeth.anne.spivey@gmail.com>
2024-01-25 12:16:55 +0000

    added event listener js for DOM load

---
+++ assets/js/script.js
@@ -0,0 +1,52 @@
+//wait for the DOM to finish loading before running the game
+// Get the button elements and event listeners to them
+
+document.addEventListener('DOMContentLoaded', function() {
+    let buttons = document.getElementsByTagName('button');
+    for (let button of buttons) {
+        button.addEventListener('click', function() {
+            if(this.getAttribute('data-type')=== "submit") {
+                alert("You clicked Submit!");
+                else {
+                    let gameType = this.getAttribute("data-type");
+                    alert(`You clicked £{gameType}`);
+                }
+            }
+        })
+    }
+})
+
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer(){
+
+}
+
+function incrementScore(){
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubstractQuestion() {
+
+}
+
+function displayMulitplyQuestion(){
+
+}
+
+
+
